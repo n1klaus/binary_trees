a65092540bb719a0a8d8cf3d8f51ed46f4ad20e7
Nick Nyanjui <Nicknyanjui@gmail.com>
2022-09-23 23:17:31 +0300

    Update 3-binary_tree_delete.c

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -2,7 +2,7 @@
 /**
  * binary_tree_delete - deletes an entire binary tree
  * @tree : pointer to the root node of the tree to search
- * 
+ *
  * Return: Nothing
  */
 void binary_tree_delete(binary_tree_t *tree)
